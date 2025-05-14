c6f67ff4242edcee037910a224b4b6349676479e
Sovannara Hong <rapewpew@gmail.com>
2025-05-15 01:18:30 +0700

    update

--- src/page/js/cardData.js
+++ src/page/js/cardData.js
@@ -41,53 +41,53 @@ cardImg();
 
 const ContainerTotal = document.querySelector("#Container-card-promis");
 
-// function CardPro() {
-//   fetch("https://dummyjson.com/products")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       ContainerTotal.innerHTML = data.products
-//         .map(
-//           (pros) => `
-//     <div class="xl:min-w-[300px] lg:min-w-[250px] md:min-w-[200px] sm:min-w-[190px] min-w-[150px]  relative bg-[#f5f5f5] overflow-hidden">
-//             <div id="discount" >
-//               <p
-//                 class="absolute top-0 text-white bg-red-500 rounded-[5px] m-3 px-[10px] py-[3px] "
-//               >
-//                 -41%
-//               </p>
-//             </div>
-//             <img
-//               class=" xl:h-[400px] lg:h-[350px] md:h-[300px] sm:h-[260px] h-[200px] overflow-hidden"
-//               src=${pros.images[0]}
-//               alt=""
-//             />
-//             <div class="flex justify-between px-[15px] pt-[15px]">
-//             <div class="xl:flex xl:gap-[10px] lg:flex md:gap-[10px] md:flex lg:gap-[10px] ">
-//               <p class="text-red-500 font-semibold ">
-//                 US $${pros.price}
-//               </p>
-//               <del class="text-black font-normal">US $19.9</del>
-//               </div>
-//                <div>
-//            <i class="fa-regular fa-heart icon-heart cursor-pointer"></i>
-//  </div>
-//             </div>
-//             <p id="title" class="font-semibold px-[15px] truncate sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis">
-//   Unisex T-Shirts With Printed
-// </p>
+function CardPro() {
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      ContainerTotal.innerHTML = data.products
+        .map(
+          (pros) => `
+    <div class="xl:min-w-[300px] lg:min-w-[250px] md:min-w-[200px] sm:min-w-[190px] min-w-[150px]  relative bg-[#f5f5f5] overflow-hidden">
+            <div id="discount" >
+              <p
+                class="absolute top-0 text-white bg-red-500 rounded-[5px] m-3 px-[10px] py-[3px] "
+              >
+                -41%
+              </p>
+            </div>
+            <img
+              class=" xl:h-[400px] lg:h-[350px] md:h-[300px] sm:h-[260px] h-[200px] overflow-hidden"
+              src=${pros.images[0]}
+              alt=""
+            />
+            <div class="flex justify-between px-[15px] pt-[15px]">
+            <div class="xl:flex xl:gap-[10px] lg:flex md:gap-[10px] md:flex lg:gap-[10px] ">
+              <p class="text-red-500 font-semibold ">
+                US $${pros.price} 
+              </p>
+              <del class="text-black font-normal">US $19.9</del>
+              </div>
+               <div>
+           <i class="fa-regular fa-heart icon-heart cursor-pointer"></i>
+ </div>
+            </div>
+            <p id="title" class="font-semibold px-[15px] truncate sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis">
+  Unisex T-Shirts With Printed
+</p>
 
-//             <div class="flex gap-[10px] px-[15px] py-[10px]">
-//               <div class="bg-red-500 w-[15px] rounded-[3px] h-[15px]"></div>
-//               <div class="bg-black w-[15px] rounded-[3px] h-[15px]"></div>
-//             </div>
-//           </div>
-
-//     `
-//         )
-//         .join("");
-//     });
-// }
-// CardPro();
+            <div class="flex gap-[10px] px-[15px] py-[10px]">
+              <div class="bg-red-500 w-[15px] rounded-[3px] h-[15px]"></div>
+              <div class="bg-black w-[15px] rounded-[3px] h-[15px]"></div>
+            </div>
+          </div>
+    
+    `
+        )
+        .join("");
+    });
+}
+CardPro();
 
 ContainerTotal.addEventListener("click", (e) => {
   if (e.target.classList.contains("icon-heart")) {
