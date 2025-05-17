9349cca0bd6343e4c468ab0828a6a92109528cc2
Sovannara Hong <rapewpew@gmail.com>
2025-05-17 11:14:44 +0700

    update

--- src/page/js/cardData.js
+++ src/page/js/cardData.js
@@ -1,3 +1,18 @@
+const bgDiv = document.getElementById("logo-bg");
+const bgImages = [
+  "/src/assets/img/bg-hero.png",
+  "/src/assets/img/1.jpg",
+  "/src/assets/img/2.jpg",
+  "/src/assets/img/3.jpg",
+];
+
+let current = 0;
+
+setInterval(() => {
+  current = (current + 1) % bgImages.length;
+  bgDiv.style.backgroundImage = `url('${bgImages[current]}')`;
+}, 5000); // 5000ms = 5 seconds
+
 const product = [
   {
     img: "https://zandokh.com/image/catalog/banner/2025/ZANDO/Category%20lifestyle,%20sportlife,%20smartcasual/Update%20May/Replace/casual-wo.jpg",
@@ -39,63 +54,6 @@ const cardImg = () => {
 };
 cardImg();
 
-const ContainerTotal = document.querySelector("#Container-card-promis");
-
-function CardPro() {
-  fetch("https://dummyjson.com/products")
-    .then((res) => res.json())
-    .then((data) => {
-      ContainerTotal.innerHTML = data.products
-        .map(
-          (pros) => `
-    <div class="xl:min-w-[300px] lg:min-w-[250px] md:min-w-[200px] sm:min-w-[190px] min-w-[150px]  relative bg-[#f5f5f5] overflow-hidden">
-            <div id="discount" >
-              <p
-                class="absolute top-0 text-white bg-red-500 rounded-[5px] m-3 px-[10px] py-[3px] "
-              >
-                -41%
-              </p>
-            </div>
-            <img
-              class=" xl:h-[400px] lg:h-[350px] md:h-[300px] sm:h-[260px] h-[200px] overflow-hidden"
-              src=${pros.images[0]}
-              alt=""
-            />
-            <div class="flex justify-between px-[15px] pt-[15px]">
-            <div class="xl:flex xl:gap-[10px] lg:flex md:gap-[10px] md:flex lg:gap-[10px] ">
-              <p class="text-red-500 font-semibold ">
-                US $${pros.price} 
-              </p>
-              <del class="text-black font-normal">US $19.9</del>
-              </div>
-               <div>
-           <i class="fa-regular fa-heart icon-heart cursor-pointer"></i>
- </div>
-            </div>
-            <p id="title" class="font-semibold px-[15px] truncate sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis">
-  Unisex T-Shirts With Printed
-</p>
-
-            <div class="flex gap-[10px] px-[15px] py-[10px]">
-              <div class="bg-red-500 w-[15px] rounded-[3px] h-[15px]"></div>
-              <div class="bg-black w-[15px] rounded-[3px] h-[15px]"></div>
-            </div>
-          </div>
-    
-    `
-        )
-        .join("");
-    });
-}
-CardPro();
-
-ContainerTotal.addEventListener("click", (e) => {
-  if (e.target.classList.contains("icon-heart")) {
-    e.target.classList.toggle("fa-regular");
-    e.target.classList.toggle("fa-solid");
-  }
-});
-
 const DataJS = document.querySelector("#data-json");
 function DataJson() {
   fetch("./src/page/data/data.json")
@@ -121,7 +79,7 @@ function DataJson() {
             <div class="flex justify-between px-[15px] pt-[15px]">
             <div class="xl:flex xl:gap-[10px] lg:flex md:gap-[10px] md:flex lg:gap-[10px] ">
               <p class="text-red-500 font-semibold ">
-                US $${pros.price} 
+                US $${pros.price}
               </p>
               <del class="text-black font-normal">US $19.9</del>
               </div>
@@ -138,7 +96,7 @@ function DataJson() {
               <div class="bg-black w-[15px] rounded-[3px] h-[15px]"></div>
             </div>
           </div>
-    
+
     `
           )
           .join(""))
@@ -146,6 +104,7 @@ function DataJson() {
     .catch((error) => console.log(error));
 }
 DataJson();
+
 DataJS.addEventListener("click", (e) => {
   if (e.target.classList.contains("icon-heart")) {
     e.target.classList.toggle("fa-regular");
@@ -153,18 +112,109 @@ DataJS.addEventListener("click", (e) => {
   }
 });
 
-const bgDiv = document.getElementById("logo-bg");
+// ---------------------------------- Shose ----------------------------------
 
-const bgImages = [
-  "/src/assets/img/bg-hero.png",
-  "/src/assets/img/1.jpg",
-  "/src/assets/img/2.jpg",
-  "/src/assets/img/3.jpg",
-];
+const CardShose = document.querySelector("#Card-Shose");
 
-let current = 0;
+function ShoseCard() {
+  fetch("./src/page/data/shose.json")
+    .then((res) => res.json())
+    .then(
+      (data) =>
+        (CardShose.innerHTML = data
+          .map(
+            (pros) => `
+    <div class="xl:min-w-[300px] lg:min-w-[250px] md:min-w-[200px] sm:min-w-[190px] min-w-[150px]  relative bg-[#f5f5f5] overflow-hidden">
+            <div id="discount" >
+              <p
+                class="absolute top-0 text-white bg-red-500 rounded-[5px] m-3 px-[10px] py-[3px] "
+              >
+                -41%
+              </p>
+            </div>
+            <img
+              class=" xl:h-[400px] lg:h-[350px] md:h-[300px] sm:h-[260px] h-[200px] overflow-hidden"
+              src=${pros.image}
+              alt=""
+            />
+            <div class="flex justify-between px-[15px] pt-[15px]">
+            <div class="xl:flex xl:gap-[10px] lg:flex md:gap-[10px] md:flex lg:gap-[10px] ">
+              <p class="text-red-500 font-semibold ">
+                US $${pros.price}
+              </p>
+              <del class="text-black font-normal">US $19.9</del>
+              </div>
+               <div>
+           <i class="fa-regular fa-heart icon-heart cursor-pointer"></i>
+ </div>
+            </div>
+            <p id="title" class="font-semibold px-[15px] truncate sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis">
+  Unisex T-Shirts With Printed
+</p>
 
-setInterval(() => {
-  current = (current + 1) % bgImages.length;
-  bgDiv.style.backgroundImage = `url('${bgImages[current]}')`;
-}, 5000); // 5000ms = 5 seconds
+            <div class="flex gap-[10px] px-[15px] py-[10px]">
+              <div class="bg-red-500 w-[15px] rounded-[3px] h-[15px]"></div>
+              <div class="bg-black w-[15px] rounded-[3px] h-[15px]"></div>
+            </div>
+          </div>
+
+    `
+          )
+          .join(""))
+    )
+    .catch((error) => console.log(error));
+}
+ShoseCard();
+
+// ---------------------------- all in one----------------------------------
+
+const allInShop = document.querySelector("#allshop");
+function Shopall() {
+  fetch("./src/page/data/allone.json")
+    .then((res) => res.json())
+    .then(
+      (data) =>
+        (allInShop.innerHTML = data
+          .map(
+            (pros) => `
+    <div class="xl:min-w-[300px] lg:min-w-[250px] md:min-w-[200px] sm:min-w-[190px] min-w-[150px]  relative bg-[#f5f5f5] overflow-hidden">
+            <div id="discount" >
+              <p
+                class="absolute top-0 text-white bg-red-500 rounded-[5px] m-3 px-[10px] py-[3px] "
+              >
+                -41%
+              </p>
+            </div>
+            <img
+              class=" xl:h-[400px] lg:h-[350px] md:h-[300px] sm:h-[260px] h-[200px] overflow-hidden"
+              src=${pros.image}
+              alt=""
+            />
+            <div class="flex justify-between px-[15px] pt-[15px]">
+            <div class="xl:flex xl:gap-[10px] lg:flex md:gap-[10px] md:flex lg:gap-[10px] ">
+              <p class="text-red-500 font-semibold ">
+                US $${pros.price}
+              </p>
+              <del class="text-black font-normal">US $19.9</del>
+              </div>
+               <div>
+           <i class="fa-regular fa-heart icon-heart cursor-pointer"></i>
+ </div>
+            </div>
+            <p id="title" class="font-semibold px-[15px] truncate sm:whitespace-nowrap sm:overflow-hidden sm:text-ellipsis">
+  Unisex T-Shirts With Printed
+</p>
+
+            <div class="flex gap-[10px] px-[15px] py-[10px]">
+              <div class="bg-red-500 w-[15px] rounded-[3px] h-[15px]"></div>
+              <div class="bg-black w-[15px] rounded-[3px] h-[15px]"></div>
+            </div>
+          </div>
+
+    `
+          )
+          .join(""))
+    )
+    .catch(error);
+}
+Shopall();
--- src/page/js/script.js
+++ src/page/js/script.js
@@ -1,31 +1,33 @@
-// const header = document.querySelector("#header");
-// const searchbar = document.querySelector("#search-bar");
-// const searchtrigger = document.querySelector("#search-trigger");
-// const searchNav = document.querySelector("#navSearch");
-
-// function toggelSearch(isShow) {
-//   if (isShow) {
-//     header.classList.add("hidden");
-//     searchbar.classList.add("active");
-//   }
-// }
-// searchtrigger.addEventListener("click", () => toggelSearch(true));
-// searchNav.addEventListener("click", () => toggelSearch(true));
-
-// document.addEventListener("click", function (event) {
-//   if (!searchbar.contains(event.target) && !header.contains(event.target)) {
-//     header.classList.remove("hidden");
-//     searchbar.classList.remove("active");
-//   }
-// });
-
 let scrollRight = document.querySelector("#scroll-right");
 let scrollLeft = document.querySelector("#scroll-left");
 let PopSong = document.querySelector(".pop-song");
 
 scrollRight.addEventListener("click", () => {
-  PopSong.scrollLeft += 330; // Scroll right
+  PopSong.scrollLeft += 1230; // Scroll right
 });
 scrollLeft.addEventListener("click", () => {
-  PopSong.scrollLeft -= 330; // Scroll left
+  PopSong.scrollLeft -= 1230; // Scroll left
+});
+
+let leftScroll = document.querySelector("#left-scroll");
+let rightScroll = document.querySelector("#right-scroll");
+let ListSong = document.querySelector(".List-song");
+
+rightScroll.addEventListener("click", () => {
+  ListSong.scrollLeft += 1230; // Scroll right
+});
+
+leftScroll.addEventListener("click", () => {
+  ListSong.scrollLeft -= 1230; // Scroll left
+});
+
+let ListAll = document.querySelector(".List-all");
+let leftScrolls = document.querySelector("#left-scrolls");
+let rightScrolls = document.querySelector("#right-scrolls");
+rightScrolls.addEventListener("click", () => {
+  ListAll.scrollLeft += 1230; // Scroll right
+});
+
+leftScrolls.addEventListener("click", () => {
+  ListAll.scrollLeft -= 1230; // Scroll left
 });
